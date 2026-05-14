a142fa4d94443ff0553b4c7f4c01ba2569c42e2e
Jihee Ryu <chrisjihee@etri.re.kr>
2026-05-14 18:23:56 +0900

    Update setup-main, version-dep

--- setup-main.sh
+++ setup-main.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 # 4. Create a new environment
 t1=$SECONDS; echo -e "\n[$(date +'%Y-%m-%d %H:%M:%S')] 4. Creating new environment..."
 deactivate 2>/dev/null || true; rm -rf .venv; rm -rf *.egg-info;
@@ -6,39 +8,45 @@ uv venv .venv --python 3.12 --python-preference only-managed --clear
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] 4. Created new environment (Elapsed: $((SECONDS - t1))s)"
 
 # 5. Install the required packages
-t1=$SECONDS; echo -e "\n[$(date +'%Y-%m-%d %H:%M:%S')] 5. Installing standard required packages..."
+t1=$SECONDS; echo -e "\n[$(date +'%Y-%m-%d %H:%M:%S')] 5. Installing required packages..."
 source .venv/bin/activate; uv pip list
+uv pip install cmake ninja wheel packaging setuptools setuptools_scm
 
-uv pip install cmake ninja wheel packaging setuptools_scm
+# torch 설치
+t0=$SECONDS; echo -e "\n[$(date +'%Y-%m-%d %H:%M:%S')] Installing torch..."
+uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] torch installed (Elapsed: $((SECONDS - t0))s)"
 
-rm -rf lm-evaluation-harness
-git clone --depth 1 https://github.com/EleutherAI/lm-evaluation-harness
-
-# CUDA 12.8용 torch를 먼저 고정
-uv pip install torch torchvision torchaudio \
-  --index-url https://download.pytorch.org/whl/cu128
+# vllm 설치
+t0=$SECONDS; echo -e "\n[$(date +'%Y-%m-%d %H:%M:%S')] Installing vllm..."
+MAX_JOBS=$(nproc) uv pip install vllm --no-binary vllm --no-build-isolation --extra-index-url https://download.pytorch.org/whl/cu128 --index-strategy unsafe-best-match
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] vllm installed (Elapsed: $((SECONDS - t0))s)"
 
 # lm-eval 설치
-uv pip install -e ./lm-evaluation-harness \
-  --extra-index-url https://download.pytorch.org/whl/cu128 \
-  --index-strategy unsafe-best-match
+t0=$SECONDS; echo -e "\n[$(date +'%Y-%m-%d %H:%M:%S')] Installing lm-eval..."
+rm -rf lm-evaluation-harness
+git clone --depth 1 https://github.com/EleutherAI/lm-evaluation-harness
+uv pip install -e ./lm-evaluation-harness --extra-index-url https://download.pytorch.org/whl/cu128 --index-strategy unsafe-best-match
+uv pip install "lm_eval[hf]" "lm_eval[api]" "lm_eval[vllm]" --extra-index-url https://download.pytorch.org/whl/cu128 --index-strategy unsafe-best-match
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] lm-eval installed (Elapsed: $((SECONDS - t0))s)"
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] 5. Installed required packages (Elapsed: $((SECONDS - t1))s)"
 
-uv pip install "lm_eval[hf]" "lm_eval[api]" \
-  --extra-index-url https://download.pytorch.org/whl/cu128 \
-  --index-strategy unsafe-best-match
-
-# uv pip install "lm_eval[vllm]"
-
-
+# 6. Check the installed packages and their versions
 python - <<'PY'
-import torch
+import sys
+import torch, vllm
+
 print("- torch:", torch.__version__)
 print("- torch.version.cuda:", torch.version.cuda)
 print("- cuda available:", torch.cuda.is_available())
 print("- device count:", torch.cuda.device_count())
 if torch.cuda.is_available():
     print("- device 0:", torch.cuda.get_device_name(0))
-    print("cuda tensor:", torch.tensor([1.0], device="cuda"))
+    print("- cuda tensor:", torch.tensor([1.0], device="cuda"))
+
+print("- vllm:", vllm.__version__)
+from vllm import LLM, SamplingParams
+print("- import vllm.LLM OK")
 PY
 
 uv pip list > version-dep.txt; uv pip list | grep -E "torch|llm|deepspeed|attn|peft|transformer|accelerate|huggingface|datasets|pandas|numpy|chris|prog"
