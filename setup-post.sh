3954cac0033949cbbd0ce6fab578829ee3e29507
Jihee Ryu <chrisjihee@etri.re.kr>
2026-06-28 22:09:22 +0900

    git clone https://github.com/gsma-labs/evals gsma-evals

--- setup-post.sh
+++ setup-post.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/EleutherAI/lm-evaluation-harness
 
 # 7-2. Clone GSMA-Evals (for Task Implementation)
-git clone https://github.com/gsma-labs/evals
+git clone https://github.com/gsma-labs/evals gsma-evals
 
 # 8. Link HF cache and login to HF
 shopt -s globstar
